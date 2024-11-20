28044f4035be2d8cc74a9c5101f394528c2fe201
Melani <mvargas30424@ufide.ac.cr>
2024-11-20 01:44:15 -0600

    Homepage Parte I

---
+++ CosaVerde/src/main/java/com/CosaVerde/com/CosaVerdeApplication.java
@@ -0,0 +1,13 @@
+package com.CosaVerde.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosaVerdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosaVerdeApplication.class, args);
+	}
+
+}
---
+++ CosaVerde/src/test/java/com/CosaVerde/com/CosaVerdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CosaVerde.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosaVerdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
